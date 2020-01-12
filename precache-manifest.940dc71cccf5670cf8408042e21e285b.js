b4e872fbde298bc287c04735888692ba35c1c873
Sebastian Blissett <sebass27@gmail.com>
2020-01-12 15:57:31 -0800

    Updates

---
+++ precache-manifest.940dc71cccf5670cf8408042e21e285b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34cf3a32575c9e1a5d7d66ee38e7165d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc750023ebc97337b43c",
+    "url": "/static/css/main.ea1f732e.chunk.css"
+  },
+  {
+    "revision": "0354e0960e0d861dc049",
+    "url": "/static/js/2.f161beac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f161beac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc750023ebc97337b43c",
+    "url": "/static/js/main.b2332ca8.chunk.js"
+  },
+  {
+    "revision": "919ccb9ed376d87d0ca6",
+    "url": "/static/js/runtime-main.27a28147.js"
+  }
+]);
