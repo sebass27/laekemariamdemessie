6f4da61177bc23e6a8a68d0c241683c64d8f3e2e
sebass27 <sebass27@users.noreply.github.com>
2020-01-13 02:19:03 +0000

    Deploying to gh-pages from  4a77b07ef026b560807e676a7806ff07c9cb9ac1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/laekemariamdemessie/precache-manifest.c195655937e755d86e81bd46fa8801cb.js"
+  "/precache-manifest.0c5889c1ed9dd9a255798fbcfcc57554.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/laekemariamdemessie/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
