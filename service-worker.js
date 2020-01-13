94239b0dcc30a5fae79072209462560281f6d867
sebass27 <sebass27@users.noreply.github.com>
2020-01-13 02:36:21 +0000

    Deploying to gh-pages from  21089c1554e0d1f46f078a3493d18c00948022fb

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.940dc71cccf5670cf8408042e21e285b.js"
+  "/precache-manifest.0c5889c1ed9dd9a255798fbcfcc57554.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
