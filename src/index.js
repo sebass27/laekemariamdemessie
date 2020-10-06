7e59bfd4c9f884883c9593afb217f91eec4dcaea
Sebastian Blissett <enmayopiniogn@gmail.com>
2020-10-06 00:37:10 -0700

    Add Google Analytics to track page visits

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-179688309-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
