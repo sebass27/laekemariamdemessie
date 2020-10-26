98f6bb5316808d0d921e8eb485cfb612eb5d0eb3
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2020-10-26 11:15:50 -0700

    added an npm for a footer that shows up informing visitors that the website collects cookies. The EU has laws against collecting information without consent.

--- src/components/page_components/cookie-consent.js
+++ src/components/page_components/cookie-consent.js
@@ -3,21 +3,23 @@ import React, { Component } from "react";
 import CookieConsent, { Cookies } from "react-cookie-consent";
 
 class Consent extends Component {
+    
     render(){
+        Cookies.set("test", "nice"); 
         return (
-
-            <CookieConsent
-                location="bottom"
-                buttonStyle= {{ color: "#4e503b", fontSize: "13px" }}
-                style= {{ background: "#2B373B" }} 
-                expires={150}
-            > 
-                
-            This website uses cookies to enhance the user experience.
-            </CookieConsent>
+            <React.Fragment>
+                <CookieConsent
+                    enableDeclineButton
+                    location="bottom"
+                    buttonStyle= {{ color: "#4e503b", fontSize: "13px" }}
+                    style= {{ background: "#2B373B" }} 
+                    expires={150}
+                > 
+                This website uses cookies to enhance the user experience.
+                </CookieConsent>
+            </React.Fragment>
         )
     }
-    
 }
 
 export default Consent;
