3a62270318adc1cdbc078d839f69eb16aea1856b
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2020-12-15 12:37:08 -0800

    made changes

--- src/components/listings/Card.js
+++ src/components/listings/Card.js
@@ -6,15 +6,15 @@ class Card extends React.Component {
       <div className="row">
         <div className="col s12 m10 l8">
           <div className= "card">
-            <div className="card horizontal">
-              <div className="card-image grey darken-4">
+            {/* <div className="card horizontal"> */}
+              {/* <div className="card-image grey darken-4">
                 <img src={this.props.imageLink} alt={this.props.title} width="200px" height="60px"/>
-              </div>
+              </div> */}
               <div className="card-content yellow accent-1">
                 <span className="card-title">{this.props.title}</span>
                 <span className= "flow-text">{this.props.text}</span>
               </div>
-            </div>
+            {/* </div> */}
             <div className="card-action purple">
                 <a href={this.props.pageLink} className="link">Link to Article</a>
             </div>
